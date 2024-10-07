83725c9e2903b9ddca3b491e26caaf46cef365bd
Nikolay Yaroshenko <nik.olay.93@mail.ru>
2024-10-07 15:07:16 +0300

    split onboarding into separate page

---
+++ src/app/onboarding/page.tsx
@@ -0,0 +1,104 @@
+"use client";
+
+import { useInvoke, useInvokeMutate } from "@/api/useInvoke";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { open } from "@tauri-apps/plugin-dialog";
+import Link from "next/link";
+
+export default function Home() {
+  const { data: installPath, mutate: mutateInstallPath } = useInvoke(
+    "get_install_path",
+    undefined
+  );
+  const { data: searchPathsState, mutate: mutateSearchPathsState } = useInvoke(
+    "get_search_paths_state",
+    undefined
+  );
+
+  const { trigger: setInstallPath } = useInvokeMutate("set_install_path");
+  const { trigger: modSearchPaths } = useInvokeMutate("mod_search_paths");
+
+  return (
+    <div className="p-6 flex flex-col gap-8 h-screen w-screen">
+      <span className="text-2xl font-bold">
+        Welcome to Addon Manager! Let's get you set up
+      </span>
+
+      <div className="grid grid-cols-2 items-center gap-6">
+        <div className="flex flex-col gap-1">
+          <span className="font-bold text-lg">Addon install path</span>
+          <span className="text-primary-200 text-sm">
+            The path where all your addons will be stored
+          </span>
+        </div>
+        <Input
+          type="text"
+          className="cursor-pointer text-lg h-10"
+          value={installPath ?? "Select..."}
+          readOnly
+          onClick={() => {
+            open({
+              directory: true,
+              multiple: false,
+              canCreateDirectories: true,
+              title: "Select addons install path",
+            }).then((result) => {
+              if (result !== null) {
+                setInstallPath({ installPath: result });
+                mutateInstallPath(undefined, {
+                  populateCache: false,
+                  revalidate: true,
+                  optimisticData: {
+                    success: true,
+                    result,
+                  },
+                });
+              }
+            });
+          }}
+        />
+
+        <span className="text-lg font-bold">
+          <span className="text-primary-200">gameinfo.gi</span>
+          <span> state</span>
+        </span>
+
+        <div className="flex flex-row gap-4 items-center">
+          <span className="text-lg">{searchPathsState}</span>
+          <Button
+            className="ml-auto"
+            disabled={searchPathsState !== "Vanilla"}
+            onClick={() => {
+              modSearchPaths(undefined);
+              mutateSearchPathsState(undefined, {
+                populateCache: false,
+                revalidate: true,
+                optimisticData: {
+                  success: true,
+                  result: "Modded",
+                },
+              });
+            }}
+          >
+            Apply mod
+          </Button>
+        </div>
+      </div>
+
+      <Link href="/addons" legacyBehavior>
+        <Button
+          disabled={
+            installPath === undefined ||
+            searchPathsState === undefined ||
+            searchPathsState === "Vanilla"
+          }
+          className="flex gap-1 items-center mt-auto"
+        >
+          <span className="text-lg">Continue</span>
+          <span className="icon-[lucide--move-right] size-6" />
+        </Button>
+      </Link>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,16 @@
 "use client";
 
-import { useInvoke, useInvokeMutate } from "@/api/useInvoke";
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { open } from "@tauri-apps/plugin-dialog";
-import Link from "next/link";
+import { useInvoke } from "@/api/useInvoke";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 
 export default function Home() {
-  const {
-    isLoading: installPathLoading,
-    data: installPath,
-    mutate: mutateInstallPath,
-  } = useInvoke("get_install_path", undefined);
-  const {
-    isLoading: searchPathsStateLoading,
-    data: searchPathsState,
-    mutate: mutateSearchPathsState,
-  } = useInvoke("get_search_paths_state", undefined);
-
-  const [firstRender, setFirstRender] = useState(true);
-
-  const { trigger: setInstallPath } = useInvokeMutate("set_install_path");
-  const { trigger: modSearchPaths } = useInvokeMutate("mod_search_paths");
+  const { isLoading: installPathLoading, data: installPath } = useInvoke(
+    "get_install_path",
+    undefined
+  );
+  const { isLoading: searchPathsStateLoading, data: searchPathsState } =
+    useInvoke("get_search_paths_state", undefined);
 
   const router = useRouter();
 
@@ -34,106 +21,17 @@ export default function Home() {
       return;
     }
 
-    if (!firstRender) {
-      return;
-    }
-
-    setFirstRender(false);
-
     if (
       installPath !== undefined &&
       searchPathsState !== undefined &&
       searchPathsState !== "Vanilla"
     ) {
       router.push("/addons");
+    } else {
+      router.push("/onboarding");
     }
-  }, [installPath, searchPathsState, firstRender, router, isLoading]);
+  }, [installPath, searchPathsState, router, isLoading]);
 
-  if (isLoading) {
-    return <div>Loading...</div>;
-  }
-
-  return (
-    <div className="p-6 flex flex-col gap-8 h-screen w-screen">
-      <span className="text-2xl font-bold">
-        Welcome to Addon Manager! Let's get you set up
-      </span>
-
-      <div className="grid grid-cols-2 items-center gap-6">
-        <div className="flex flex-col gap-1">
-          <span className="font-bold text-lg">Addon install path</span>
-          <span className="text-primary-200 text-sm">
-            The path where all your addons will be stored
-          </span>
-        </div>
-        <Input
-          type="text"
-          className="cursor-pointer text-lg h-10"
-          value={installPath ?? "Select..."}
-          readOnly
-          onClick={() => {
-            open({
-              directory: true,
-              multiple: false,
-              canCreateDirectories: true,
-              title: "Select addons install path",
-            }).then((result) => {
-              if (result !== null) {
-                setInstallPath({ installPath: result });
-                mutateInstallPath(undefined, {
-                  populateCache: false,
-                  revalidate: true,
-                  optimisticData: {
-                    success: true,
-                    result,
-                  },
-                });
-              }
-            });
-          }}
-        />
-
-        <span className="text-lg font-bold">
-          <span className="text-primary-200">gameinfo.gi</span>
-          <span> state</span>
-        </span>
-
-        <div className="flex flex-row gap-4 items-center">
-          <span className="text-lg">{searchPathsState}</span>
-          <Button
-            className="ml-auto"
-            disabled={searchPathsState !== "Vanilla"}
-            onClick={() => {
-              modSearchPaths(undefined);
-              mutateSearchPathsState(undefined, {
-                populateCache: false,
-                revalidate: true,
-                optimisticData: {
-                  success: true,
-                  result: "Modded",
-                },
-              });
-            }}
-          >
-            Apply mod
-          </Button>
-        </div>
-      </div>
-
-      <Link href="/addons" legacyBehavior>
-        <Button
-          disabled={
-            installPath === undefined ||
-            searchPathsState === undefined ||
-            searchPathsState === "Vanilla"
-          }
-          className="flex gap-1 items-center mt-auto"
-        >
-          <span className="text-lg">Continue</span>
-          <span className="icon-[lucide--move-right] size-6" />
-        </Button>
-      </Link>
-    </div>
-  );
+  return <div>Loading...</div>;
 }
 
