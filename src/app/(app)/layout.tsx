186f1812b1dd63c5e2dd292eb864cbc62bef03b7
Nikolay Yaroshenko <nik.olay.93@mail.ru>
2024-10-07 15:08:42 +0300

    rename to addon manager

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -5,7 +5,7 @@ export default function AppLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex flex-row justify-start w-[100dvw] h-[100dvh]">
       <div className="bg-surface-500 flex flex-col gap-2 p-2 items-start">
-        <span className="font-bold py-2 text-xl self-center">ModManager</span>
+        <span className="font-bold py-2 text-xl self-center">AddonManager</span>
         <div className="h-[1px] w-full bg-surface-100/30" />
 
         <NavRoute
